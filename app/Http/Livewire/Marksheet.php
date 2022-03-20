13a0b79b6585ba89c81cdd6ff5192e69cc43efc6
Rojae Henry <rojaeh18@gmail.com>
2022-03-19 21:38:32 -0500

    update

--- app/Http/Livewire/Marksheet.php
+++ app/Http/Livewire/Marksheet.php
@@ -12,4 +12,8 @@ class Marksheet extends Component
         $mark = \App\Models\marksheet::with('trainee')->get();
         return view('livewire.marksheet', ['mark'=>$mark]);
     }
+
+    public function present($id){
+        $trainee = \App\Models\marksheet::where('id', $id)->update(['mark'=>2]);
+    }
 }
--- resources/views/livewire/marksheet.blade.php
+++ resources/views/livewire/marksheet.blade.php
@@ -8,6 +8,7 @@
                     <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Course</th>
                     <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Dorm</th>
                     <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Status</th>
+                    <th class="justify-center py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Action</th>
                 </tr>
                 </thead>
 
@@ -17,9 +18,30 @@
                         <td class="py-4 px-6 border-b border-grey-light">{{$marks->trainee->first_nm.' '.$marks->trainee->last_nm}}</td>
                         <td class="py-4 px-6 border-b border-grey-light">{{$marks->trainee->course}}</td>
                         <td class="py-4 px-6 border-b border-grey-light">{{$marks->trainee->room}}</td>
-                        <td class="py-4 px-6 border-b border-grey-light">
-                            Present
-                        </td>
+{{--                        <td class="py-4 px-6 border-b border-grey-light">--}}
+                        @if ($marks->mark === 0)
+                            <td class="py-4 px-6 border-b border-grey-light">{{ ('Pending') }}</td>
+                        @elseif($marks->mark === 1)
+                            <td class="py-4 px-6 border-b border-grey-light">{{ ('Approve') }}</td>
+                        @elseif($marks->mark === 2)
+                            <td class="pC">{{ ('Reject') }}</td>
+                            @endif
+{{--                        </td>--}}
+
+                        @if ($marks->mark === 0)
+                            <td class="flex justify-center">
+                                <form wire:submit.prevent="present" class="px-2">
+
+                                    <button type="submit"  value="1" class="btn btn-primary p-2 text-white rounded bg-black hover:bg-green-500 hover:text-black">Approve</button>
+                                </form>
+                                <form  >
+
+                                    <button type="submit"  value="2" class="btn btn-primary p-2 text-white rounded bg-black hover:bg-red-600 hover:text-black">Reject</button>
+                                </form>
+                            </td>
+                        @else
+
+                        @endif
                     </tr>
                 @endforeach
             </table>
